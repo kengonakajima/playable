74e2826de452007c32e509a5bc9df1605e5f62c9
Kengo Nakajima <kengo.nakajima@gmail.com>
2013-02-23 18:05:50 +0900

    basic com

--- trial/net/game.js
+++ trial/net/game.js
@@ -32,3 +32,40 @@ function loopfunc() {
   ctx.drawImage( sol_img, 50,50 + irange(0,100), 32,32 );
 
 }
+
+
+
+
+// client
+$(function(){
+  var socket = io.connect('http://127.0.0.1:1337/');
+  socket.on('connect', function() {
+    
+    $("#log").html($("#log").html() + "<br />" + 'connected');
+    
+    socket.on('info', function (data) {
+      $("#log").html($("#log").html() + "<br />" + data.msg);
+    });
+    
+    socket.on('msg', function(data){
+      var d = new Date();
+      console.log( d, d.getMilliseconds() );  
+      $("#log").html($("#log").html() + "<br />" + "<b>" + data.msg + "</b>");
+    });
+    
+    socket.on("ch", function(data){
+      print("name:", data.name );
+    });
+    
+    $("#send").click(function(){
+      var msg = $("#msg").val();
+      if(!msg){
+        alert("input your message");
+        return;
+      }
+      var d = new Date();
+      console.log( d, d.getMilliseconds() );
+      socket.emit('msg', msg);
+    });
+  });
+});
